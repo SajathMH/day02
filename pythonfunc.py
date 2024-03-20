a3801ec7911eeff4586db3d59a578935ea80d26a
SajathMH <sajathmohamth272@gmail.com>
2024-03-20 15:37:57 +0530

    question

---
+++ pythonfunc.py
@@ -0,0 +1,13 @@
+#create a function return the sum of given list
+
+
+print("Input the A")
+a=input()
+print("input the B")
+b=input()
+def sum(a,b):
+    return a+b
+print(sum(a,b))
+
+#create a boolean funtion to find a prime number ex:is_prime(10) -false
+#use the prime funtion and find the prime numbers between 1 to 30 
